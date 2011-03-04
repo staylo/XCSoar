ec6bee52c710d89bb72b2ce00945ea4640edb9aa
Simon Taylor <simon.taylor.uk@gmail.com>
2011-03-04 23:21:30 +0000

    WayPoint/WayPointRenderer: Reduces size of reachable outline at larger scales

--- src/WayPoint/WayPointRenderer.cpp
+++ src/WayPoint/WayPointRenderer.cpp
@@ -437,7 +437,7 @@ WayPointRenderer::DrawLandableSymbol(Canvas &canvas, const RasterPoint &pt,
       fill.set(reachable_terrain ? Color::GREEN : NOT_REACHABLE_TERRAIN);
       canvas.select(fill);
       DrawLandableBase(canvas, pt, way_point.is_airport(),
-                       radius + radius / fixed_two);
+                       radius + radius / fixed_four + 1);
     }
     fill.set(Color::MAGENTA);
   } else if (Appearance.IndLandable == wpLandableAltB) {
