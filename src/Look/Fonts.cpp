676b8589b60a3422c81ab19e2a1c67f9f265ae56
Simon Taylor <simon.taylor.uk@gmail.com>
2012-12-24 20:01:10 +0000

    Fonts: Deemphasise unimportant map labels
    Draw map labels in dark gray, important map labels in black.
    Don't use bold font.

--- src/Look/Fonts.cpp
+++ src/Look/Fonts.cpp
@@ -92,7 +92,7 @@ LoadAltairLogFonts()
   InitialiseLogfont(&log_cdi, _T("RasterGothicEighteenCond"), 19, true);
   InitialiseLogfont(&log_map_label, _T("RasterGothicTwelveCond"), 13);
   InitialiseLogfont(&log_map_label_important,
-                    _T("RasterGothicTwelveCond"), 13, true);
+                    _T("RasterGothicTwelveCond"), 13);
   InitialiseLogfont(&log_map, _T("RasterGothicFourteenCond"), 15);
   InitialiseLogfont(&log_map_bold, _T("RasterGothicFourteenCond"), 15, true);
   InitialiseLogfont(&log_infobox_small, _T("RasterGothicEighteenCond"), 19, true);
@@ -154,7 +154,7 @@ InitialiseLogFonts()
 
   // new font for map labels big/medium cities
   InitialiseLogfont(&log_map_label_important, GetStandardFontFace(),
-                    UPixelScalar(FontHeight * 0.39), true, true);
+                    UPixelScalar(FontHeight * 0.39), false, true);
 
   // new font for map labels
   InitialiseLogfont(&log_map, GetStandardFontFace(),
